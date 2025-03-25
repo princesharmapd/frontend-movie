9199230026e1bf2a433a75e1d2b82d1385f123f9
princesharmapd <114358336+princesharmapd@users.noreply.github.com>
2025-03-25 11:50:17 +0530

    Update TorrentStreamingAppBar.jsx

--- src/TorrentStreamingAppBar.jsx
+++ src/TorrentStreamingAppBar.jsx
@@ -29,14 +29,18 @@ const TorrentStreamingAppBar = () => {
       }
     };
 
-    fetchHealthData();
+    fetchHealthData(); // Initial fetch
+
+    const interval = setInterval(fetchHealthData, 300000); // Fetch every 5 minutes
+
+    return () => clearInterval(interval); // Cleanup on unmount
   }, []);
 
   return (
     <GradientAppBar position="fixed" sx={{ height: 48, margin: 0, padding: 0, top: -5, borderRadius: 10 }}>
       <Toolbar sx={{ minHeight: 48, height: 48, marginTop: "-5px" }}>
         <img src={logo} alt="Logo" style={{ height: 40, marginRight: 20 }} />
-        <Typography variant="h6" sx={{ flexGrow: 1, fontSize: "0.875rem",color:"black",fontWeight:"bold" }}>
+        <Typography variant="h6" sx={{ flexGrow: 1, fontSize: "0.875rem", color: "black", fontWeight: "bold" }}>
           Movies Streaming
         </Typography>
         {version && uptime && (
